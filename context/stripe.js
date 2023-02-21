20a5721e3afc5c9f79b6536e7a7757d0f24ac045
ttcchub <79540594+ttcchub@users.noreply.github.com>
2023-02-21 19:59:14 +0200

    Update stripe.js

--- context/stripe.js
+++ context/stripe.js
@@ -1,40 +1,42 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    // console.log(req.body.cartItems)
+
+    
     try {
       const promotionCode = await stripe.promotionCodes.create({
         coupon: 'YxKefJOE',
         code: 'VIPCODE',
       });
+      
       const params = {
-        submit_type: 'pay',
+        submit_type: 'pay', 
         mode: 'payment',
         allow_promotion_codes: true,
         payment_method_types: ['card'],
-        billing_address_collection: 'auto',
+        billing_address_collection: 'required',     
         shipping_options: [
-          { shipping_rate: 'shr_1MHC9lLX27mVeMm8KRhLRTIK' },
-          { shipping_rate: 'shr_1MHCAgLX27mVeMm8gjHhhnP2' },
+          { shipping_rate: 'shr_1MacBSLX27mVeMm892noFsut' },
+          { shipping_rate: 'shr_1MadSPLX27mVeMm82Q8KjntT' },
+          { shipping_rate: 'shr_1MacBeLX27mVeMm8aI0Lya4h' },
         ],
         line_items: req.body.map((item) => {
-          const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/0ugojw0h/production/').replace('-webp', '.webp');
 
           return {
-            price_data: {
-              currency: 'usd',
-              product_data: {
+            price_data: { 
+              currency: 'EUR',
+              
+              product_data: { 
                 name: item.name,
-                images: [newImage],
               },
               unit_amount: item.price * 100,
             },
             adjustable_quantity: {
-              enabled: true,
+              enabled:true,
               minimum: 1,
             },
             quantity: item.quantity
@@ -42,17 +44,17 @@ export default async function handler(req, res) {
         }),
         allow_promotion_codes: true,
         success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/canceled`,
+        cancel_url: `${req.headers.origin}/` ,
       }
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create(params);
+       // Create Checkout Sessions from body params.
+       const session = await stripe.checkout.sessions.create(params);
 
-      res.status(200).json(session);
-    } catch (err) {
-      res.status(err.statusCode || 500).json(err.message);
-    }
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
-  }
-}
+       res.status(200).json(session);
+     } catch (err) {
+       res.status(err.statusCode || 500).json(err.message);
+     }
+   } else {
+     res.setHeader('Allow', 'POST');
+     res.status(405).end('Method Not Allowed');
+   }
+ }
